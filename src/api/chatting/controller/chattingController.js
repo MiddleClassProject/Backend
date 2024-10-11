9e4359193865ab4bea14116a381453c79ea3ccc1
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2024-10-11 11:08:41 +0900

    채팅 관련 crud

--- src/api/chatting/controller/chattingController.js
+++ src/api/chatting/controller/chattingController.js
@@ -1,6 +1,7 @@
 const pool = require('../../../../config/databaseSet');
 const chattingService = require('../service/chattingService');
 
+//채팅방 생성
 const createRoom = (data) => {
 
     const sendId = data.sendId;
@@ -16,12 +17,48 @@ const createRoom = (data) => {
     
 }
 
+//채팅 작성
 const postMessage = (room_id, sender_id, message) => {
     chattingService.postMessage(room_id, sender_id, message);
 }
 
-const getChattingRoom = (user_id) => {
+//채팅방 조회
+const getChattingRoom = (req, res) => {
+    const user_id = req.body.user_id;
 
+    let result = chattingService.getChattingRoom(user_id);
+
+    if(!result){
+        res.status(200).send({success: true, data:result});
+    }else{
+        res.status(500).send({success: false, message: "채팅방 목록을 조회할 수 없습니다."});
+    }
 }
 
-module.exports = {createRoom, postMessage, getChattingRoom};
+//채팅 조회
+const getChatDetail = (req, res) => {
+    const room_id = req.params;
+
+    let result = chattingService.getChatDetail(room_id);
+
+    if(!result){
+        res.status(200).send({success: true, data:result});
+    }else{
+        res.status(500).send({success: false, message: "채팅 목록을 조회할 수 없습니다."});
+    }
+}
+
+//채팅방 삭제
+const deleteChat = (req, res) => {
+    const room_id = req.params;
+
+    let result = chattingService.deleteChat(room_id);
+
+    if(!result) {
+        res.status(200).send({success: true, message: "성공적으로 삭제하였습니다."});
+    }else{ 
+        res.status(500).send({success: false, message: "채팅방 삭제에 실패하였습니다."});
+    }
+}
+
+module.exports = {createRoom, postMessage, getChattingRoom, getChattingRoom, getChatDetail, deleteChat};
--- src/api/chatting/service/chattingService.js
+++ src/api/chatting/service/chattingService.js
@@ -1,57 +1,98 @@
-// const pool = require('../../../../config/databaseSet');
+const pool = require('../../../../config/databaseSet');
 
-// //새로운 방 생성
-// const createRoom = async (sendId, receiveId) => {
+//새로운 방 생성
+const createRoom = async (sendId, receiveId) => {
 
-//     let sql = `INSERT INTO chatroom (user, professor) values (?,?,NOW())`;
+    let sql = `INSERT INTO chatroom (user, professor) values (?,?,NOW())`;
 
-//     try{
-//         const [result] = await pool.query(sql, [sendId, receiveId]);
-//         return result.insertId;
-//     }catch(error){
-//         console.log(error);
-//         return error;
-//     }
+    try{
+        const [result] = await pool.query(sql, [sendId, receiveId]);
+        return result.insertId;
+    }catch(error){
+        console.log(error);
+        return error;
+    }
 
-// }
+}
 
-// //채팅방이 존재하는지 확인
-// const checkChat = async (sendId, receiveId) => {
+//채팅방이 존재하는지 확인
+const checkChat = async (sendId, receiveId) => {
 
-//     let sql = `SELECT chat_id from chatroom where user = ? and professor = ?`;
+    let sql = `SELECT chat_id from chatroom where user = ? and professor = ?`;
 
-//     try{
-//         let [result] = await pool.query(sql, [sendId, receiveId]);
+    try{
+        let [result] = await pool.query(sql, [sendId, receiveId]);
         
-//         if(result.length == 0){
-//             result[0].exists = false;
-//         }else{
-//             result[0].exists = true;
-//         }
+        if(result.length == 0){
+            result[0].exists = false;
+        }else{
+            result[0].exists = true;
+        }
 
-//         return result;
+        return result;
 
-//     }catch(error){
-//         console.log(error);
-//     }
-// }
+    }catch(error){
+        console.log(error);
+    }
+}
 
-// const postMessage = async (chatRoomId, userId, message) => {
+const postMessage = async (chatRoomId, userId, message) => {
 
-//     let sql = `INSERT INTO chat (room_id, sender_id, message) VALUES (?, ?, ?)`;
-
-
-//     try{
-//         // 메시지를 DB에 저장
-//         await db.query(sql,[chatRoomId, userId, message]);
-
-//         return true;
-//     }catch(error){
-//         console.log(error);
-//         return false;
-//     }
-
-// }
+    let sql = `INSERT INTO chat (room_id, user_id1, message) VALUES (?, ?, ?)`;
 
 
-// module.exports = {createRoom, checkChat, postMessage};
+    try{
+        // 메시지를 DB에 저장
+        await db.query(sql,[chatRoomId, userId, message]);
+
+        return true;
+    }catch(error){
+        console.log(error);
+        return false;
+    }
+
+}
+
+const getChattingRoom = async(user_id) => {
+
+    let sql =`SELECT user_id1, user_id2 from chatroom where user_id1 = ? or user_id2 = ?`;
+    
+    try{
+        let [result] = await pool.query(sql, [user_id, user_id]);
+        return result;
+    }catch(error){
+        console.log(error);
+        return undefined;
+    }
+}
+
+
+const getChatDetail = async (room_id) => {
+    let sql = `SELECT chat_id, user_id1, user_id2, message, date from chat where room_id = ?`;
+
+    try{
+        let [result] = await pool.query(sql, [room_id]);
+
+        return result;
+    }catch(error){
+        console.log(error);
+
+        return undefined;
+    }
+}
+
+const deleteChat = async (room_id) => {
+    let sql = `DELETE from chatroom where room_id = ?; DELETE from chat where room_id = ?`;
+
+    try{
+        let result = await pool.query(sql, [room_id, room_id]);
+
+        return result;
+    }catch(error){
+        console.log(error);
+
+        return undefined;
+    }   
+}
+
+module.exports = {createRoom, checkChat, postMessage, getChattingRoom,getChatDetail,deleteChat};
