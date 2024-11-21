8d70ea02da6dc3d74a6777cf3bed303018954d12
정현성 <165395044+dau-J@users.noreply.github.com>
2024-11-22 04:08:46 +0900

    Add files via upload

---
+++ Backend/src/routes/professor/professorRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const professorController = require('../../api/professor/controller/professorController');
+
+// 교수 목록보기
+router.get('/', professorController.professorList);
+
+// 교수 상세보기 
+router.get('/:id', professorController.professorDetail);
+
+// 교수 탈퇴
+router.delete("/quit", professorController.professorQuit); // todo: professorId 추가 필요
+
+// 교수 즐겨찾기 토글
+router.post('/:id/star', professorController.toggleStar);
+
+module.exports = router;
