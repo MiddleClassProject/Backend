f85b1d13924ee57d4f307f46334e5340b9621504
정현성 <165395044+dau-J@users.noreply.github.com>
2024-11-22 04:09:11 +0900

    Delete Backend directory

--- Backend/review/routes/reviewRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getProfessors, submitReview, getReviewList } = require('../controller/reviewController');
-const router = express.Router();
-
-router.get('/professors', getProfessors);
-router.get('/list', getReviewList);
-router.post('/submit', submitReview);
-
-module.exports = router;
