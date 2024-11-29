3e8212a3d0c53477f9830db167a2a96be8bc9e7a
Kim Min JU <94983600+KMJ-KimMinJu@users.noreply.github.com>
2024-11-29 10:12:24 +0900

    css 파일 분리

--- src/routes/charttingRoute.js
+++ src/routes/charttingRoute.js
@@ -6,14 +6,15 @@ const socketIO = require("socket.io");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require('path');
 const chattingController = require('../api/chatting/controller/chattingController');
 
 router.get("/", (req, res) => {
-  res.sendFile("/Users/kimminju/Desktop/MiddleClassBackend/Backend/" + "view/chat.html");
+  res.sendFile(path.join(__dirname, "/../../view/chat.html"));
 });
 
 router.get("/payment", (req, res) => {
-  res.sendFile("/Users/kimminju/Desktop/MiddleClassBackend/Backend/" + "view/payment.html")
+  res.sendFile(path.join(__dirname, "/../../view/payment.html"))
 });
 
 //채팅방 목록 조회
--- src/server.js
+++ src/server.js
@@ -63,7 +63,8 @@ app.use("/routes", proRouter);
 
 //현성
 app.use("/api/review", reviewRouter); 
-app.use("/chat", express.static(path.join(__dirname, "../view"))); 
+
+app.use("/chat", express.static(path.join(__dirname, "/../view"))); 
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
